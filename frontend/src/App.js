d5da14858b43a6ef4b6f41d40f34b97dd0b476da
pocco <ssilver121@naver.com>
2024-09-02 14:09:37 +0900

    fix: 닉네임 변경 후 페이지 새로고침 하지 않도록 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,32 +6,44 @@ import Recommend from "./pages/user/Recommend";
 import Qna from "./pages/user/Qna";
 import Login from "./pages/user/Login";
 import CssBaseline from "@mui/material/CssBaseline";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewQna from "./pages/user/NewQna";
 import SignUp from "./pages/user/SignUp";
 import SignUpKakao from "./pages/user/SignUpKakao";
 import QnaDetail from "./pages/user/QnaDetail";
 import EditQna from "./pages/user/EditQna";
 import AdminQnaDetail from "./pages/admin/AdminQnaDetail";
-import MyPage from "./pages/user/MyPage";
 import AdminUsers from "./pages/admin/AdminUsers";
 import AdminQna from "./pages/admin/AdminQna";
+import MyPage from "./pages/user/MyPage";
+
+function getCookie(name) {
+    const matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([.$?*|{}()[]\\\/+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
 function App() {
     const location = useLocation();
     const isAdminRoute = location.pathname.startsWith('/admin');
+    const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        setNickname(getCookie("nickname"));
+    }, [nickname]);
 
     return (
         <div>
             <CssBaseline />
-            {isAdminRoute ? <AdminHeader /> : <Header />}
+            {isAdminRoute ? <AdminHeader /> : <Header nickname={nickname}/>}
             <Routes>
                 {/* 일반 사용자 경로 */}
                 <Route path="/" element={<Recommend/>}/>
                 <Route path="/recommend" element={<Recommend/>}/>
                 <Route path="/qnas" element={<Qna/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/mypage" element={<MyPage/>}/>
+                <Route path="/login" element={<Login setNickname={setNickname}/>}/>
+                <Route path="/mypage" element={<MyPage nickname={nickname} setNickname={setNickname}/>}/>
                 <Route path="/qnas/new" element={<NewQna/>}/>
                 <Route path="/sign-up" element={<SignUp/>}/>
                 <Route path="/sign-up-kakao" element={<SignUpKakao/>}/>
